f4b9a8943952d623535e7369bd3f0dcf0493de65
Gonzalo Campos <erick.campos@wizeline.com>
2020-06-04 20:22:52 -0500

    Experiment with styles.

--- App.js
+++ App.js
@@ -1,14 +1,29 @@
 import React from 'react';
-import { View, Text, SafeAreaView } from 'react-native';
+import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
 
 const App = () => {
   return (
-    <SafeAreaView>
-      <View>
+    <SafeAreaView style={styles.safeArea}>
+      <View style={[styles.container, styles.pink]}>
         <Text>Hello, world!</Text>
       </View>
     </SafeAreaView>
   )
 };
+
+const styles = StyleSheet.create({
+  pink:  {
+    backgroundColor: 'pink',
+  },
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'pink',
+    flex: 1
+  },
+  safeArea: {
+    flex: 2
+  }
+});
 
 export default App;
