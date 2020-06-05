defc5242362f837745ea1d9cccd3c477f551db72
Gonzalo Campos <erick.campos@wizeline.com>
2020-06-04 21:48:31 -0500

    Use FlatList instead of hardcoding the color boxes

--- App.js
+++ App.js
@@ -1,22 +1,36 @@
 import React from 'react';
-import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
+import { View, Text, SafeAreaView, StyleSheet, FlatList } from 'react-native';
 
 import ColorBox from './components/ColorBox';
 
+const COLORS = [
+  { colorName: 'Base03', hexCode: '#002b36' },
+  { colorName: 'Base02', hexCode: '#073642' },
+  { colorName: 'Base01', hexCode: '#586e75' },
+  { colorName: 'Base00', hexCode: '#657b83' },
+  { colorName: 'Base0', hexCode: '#839496' },
+  { colorName: 'Base1', hexCode: '#93a1a1' },
+  { colorName: 'Base2', hexCode: '#eee8d5' },
+  { colorName: 'Base3', hexCode: '#fdf6e3' },
+  { colorName: 'Yellow', hexCode: '#b58900' },
+  { colorName: 'Orange', hexCode: '#cb4b16' },
+  { colorName: 'Red', hexCode: '#dc322f' },
+  { colorName: 'Magenta', hexCode: '#d33682' },
+  { colorName: 'Violet', hexCode: '#6c71c4' },
+  { colorName: 'Blue', hexCode: '#268bd2' },
+  { colorName: 'Cyan', hexCode: '#2aa198' },
+  { colorName: 'Green', hexCode: '#859900' },
+];
+
 const App = () => {
   return (
-    <SafeAreaView>
-      <View style={styles.container}>
-        <Text style={
-          [styles.textMargin, 
-           styles.titleText]}>
-          Here are some boxes of different colours
-        </Text>
-        <ColorBox colorName='Cyan' hexCode='#2aa198' />
-        <ColorBox colorName='Blue' hexCode='#268bd2' />
-        <ColorBox colorName='Magenta' hexCode='#d33682' />
-        <ColorBox colorName='Orange' hexCode='#cb4b16' />
-      </View>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={COLORS}
+        keyExtractor={(item, index) => item.hexCode}
+        renderItem={({ item }) => <ColorBox colorName={item.colorName} hexCode={item.hexCode} />}
+        ListHeaderComponent={<Text style={styles.titleText}>Solarized Colors</Text>}
+      />
     </SafeAreaView>
   )
 };
@@ -28,7 +42,8 @@ const styles = StyleSheet.create({
   titleText: {
     marginTop: 40,
     marginBottom: 18,
-    fontSize: 18
+    fontSize: 18,
+    fontWeight: 'bold'
   }
 });
 
