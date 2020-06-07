2adc45e0dece56c805e3d12123d0467af999e280
Gonzalo Campos <erick.campos@wizeline.com>
2020-06-06 20:21:45 -0500

    Move flat list header to FlatList component.

--- components/screens/ColorPalette.js
+++ components/screens/ColorPalette.js
@@ -5,18 +5,20 @@ import ColorBox from '../ColorBox';
 
 const ColorPalette = ({ route, navigation }) => {
   return (
-    <FlatList
-      data={route.params.colors}
-      keyExtractor={(item, index) => item.hexCode}
-      renderItem={({ item }) => <ColorBox colorName={item.colorName} hexCode={item.hexCode} />}
-      ListHeaderComponent={<Text style={styles.titleText}>{route.params.paletteName} Colors</Text>}
-    />
+    <View style={styles.container}>
+      <FlatList
+        data={route.params.colors}
+        keyExtractor={(item, index) => item.hexCode}
+        renderItem={({ item }) => <ColorBox colorName={item.colorName} hexCode={item.hexCode} />}
+        ListHeaderComponent={<Text style={styles.titleText}>{route.params.paletteName} Colors</Text>}
+      />
+    </View>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    marginHorizontal: 20
+    marginHorizontal: 15
   },
   titleText: {
     marginTop: 40,
