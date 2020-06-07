2596646f2c034cc9bd8f2d01e0dbf16a278412fd
Gonzalo Campos <erick.campos@wizeline.com>
2020-06-07 16:40:30 -0500

    Create instructions and text input.

--- components/screens/ColorPaletteModal.js
+++ components/screens/ColorPaletteModal.js
@@ -1,12 +1,35 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, StyleSheet } from 'react-native';
 
 const ColorPaletteModal = () => {
+
+  const [paletteName, setPaletteName] = useState("");
+
   return(
-    <View>
-      <Text>Hello, I'm a modal</Text>
+    <View style={styles.container}>
+      <Text style={styles.instructions}>Name of your color palette</Text>
+      <TextInput 
+        style={styles.input} 
+        onChangeText={setPaletteName}/>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    margin: 10,
+  },
+  instructions: {
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+  input: {
+    borderWidth: 1,
+    marginVertical: 15,
+    padding: 10,
+    borderColor: '#4c4c4c',
+    borderRadius: 5,
+  },
+});
 
 export default ColorPaletteModal;
