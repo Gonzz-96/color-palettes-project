66f3570bbf03986105280f5b4361243d80d96eed
Gonzalo Campos <erick.campos@wizeline.com>
2020-06-06 19:50:43 -0500

    Create PalettePreview component.

---
+++ components/PalettePreview.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+
+const PalettePreview = ({ paletteName, colors }) => {
+  return(
+    <View style={styles.container}>
+      <Text style={styles.header}>{paletteName} Colors</Text>
+      <FlatList 
+        style={{marginTop: 10}}
+        horizontal={true}
+        data={colors.slice(0, 10)} 
+        keyExtractor={(item ) => item.hexCode}
+        renderItem={({ item }) => <View style={[styles.colorPreview, {backgroundColor: item.hexCode}]}/>} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    margin: 10
+  },
+  colorPreview: {
+    height: 30,
+    width: 30,
+    marginRight: 6
+  },
+  header: {
+    fontSize: 18,
+    fontWeight: 'bold'
+  }
+});
+
+export default PalettePreview;
